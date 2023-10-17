42654998c662c63ea2f5b5c6f99de32278ba6d15
Bence <78052125+benciea@users.noreply.github.com>
2023-10-17 09:32:51 +0200

    Add files via upload

---
+++ javito.js
@@ -0,0 +1,10 @@
+let container = document.getElementById("container");
+container.style.backgroundColor = "#12406A";
+
+let doboz = document.getElementsByClassName("tarolodoboz");
+doboz[0].style.backgroundColor = "#EB8F90";
+doboz[1].style.backgroundColor = "#FFB471";
+doboz[2].style.backgroundColor = "#ADBED2";
+
+let body = document.getElementsByTagName("body");
+body[0].style.backgroundColor = "#CFF";
